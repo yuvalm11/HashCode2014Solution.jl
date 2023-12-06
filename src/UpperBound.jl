425d7166a8d02644d33adba0759ed2fc7cb6783c
Yuval Mamana <yuvalm11@mit.edu>
2023-12-05 23:40:54 -0500

    updated docs

--- src/UpperBound.jl
+++ src/UpperBound.jl
@@ -3,6 +3,16 @@
 
 Approximately calculates the upper bound of the distance traveled by `nb_cars` cars in a given `adj_list` of streets.
 
+The algorithm works as follows:
+1. Sort the streets by decreasing speed.
+2. For each car, take the fastest street that is not already taken and that does not exceed the total duration of the city.
+3. Add the distance of the selected street to the total distance traveled by the cars.
+4. Repeat steps 2 and 3 until no street can be taken (either because the total duration is exceeded or because all streets are taken).
+
+The algorithm is not optimal since it does not take into account the connectivity of the streets. 
+However, it is fast and gives a good approximation of the upper bound since it is greedily choosing the streets that cover the most
+distance in the shortest amount of time without violating the duration constraint.
+
 # Arguments
 - `adj_list::AdjList`: The adjacency list representation of the street network.
 - `nb_cars::Int`: The number of cars.
