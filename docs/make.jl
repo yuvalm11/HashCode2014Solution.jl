f224ec00d07ac5a304f902bdf89450f5f603136a
Yuval Mamana <yuvalm11@mit.edu>
2023-12-07 20:13:19 -0500

    added docs

--- docs/make.jl
+++ docs/make.jl
@@ -15,7 +15,7 @@ makedocs(;
         assets=String[],
     ),
     pages=[
-        "Home" => "index.md", "Tutorial" => "tutorial.md", "API" => "api.md",
+        "Home" => "index.md", "Tutorial" => "tutorial.md", "API" => "api.md"
     ],
 )
 
